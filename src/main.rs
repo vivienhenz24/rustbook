2168e10cea7354926424e89e076d8a0405fff732
Vivien Henz <vhenz@college.harvard.edu>
2025-11-23 10:29:18 -0500

    new chapt

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,18 @@
-struct Rectangle {
-    width: u32,
-    height: u32,
+
+#[allow(dead_code)]
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter,
 }
 
-impl Rectangle {
-    fn area(&self) -> u32 {
-        self.width * self.height
+fn main(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime  => 10,
+        Coin::Quarter => 25,
     }
 
-    fn size_check(&self, other: &Rectangle) -> bool {
-
-        self.width > other.width && self.height > other.height
-
-    }
-}
-
-fn main() {
-    let rect1 = Rectangle { width: 30, height: 50 };
-    let rect2 = Rectangle { width: 2, height: 80};
-    println!("The area of the rectangle is {} square pixels.", rect1.area());
-    println!("Can it hold within the other rectangle: {}", rect1.size_check(&rect2));
 }
